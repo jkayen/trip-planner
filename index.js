3c7dcd549b370ef2d36e17bdede3a90e55e55ce8
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-21 14:14:37 -0400

    final before merge with solution

--- index.js
+++ index.js
@@ -9,37 +9,38 @@ const nunjucks = require('nunjucks');
 const db = require('./models');
 
 const routes = require('./routes');
+// make the top-level app
 const app = express();
-
-
+// logging
+app.use(morgan('dev'));
+// nunjucks boilerplate
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', { noCache: true });
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false } ));
+// body-parser boilerplate
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false } ));
 
-
-
+// static path for our css
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/bootstrap',express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
 app.use('/jquery',express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.use(routes);
-
+// 404 catcher
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
+// error handler
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   console.error(err);
   res.send('ERROR');
 });
 
+// listen function
 db.sync()
   .then(() => {
     app.listen(3000, () => {
